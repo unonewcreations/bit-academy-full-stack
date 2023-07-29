11b0af14384a07ad53ecc67cf09a89331e2a44f3
unonewcreations <unonewcreations@gmail.com>
2023-07-29 15:01:20 +0200

    fixing nested git

---
+++ javascript-beginner/04-level/What-if-26542838-6798abb5/script.js
@@ -0,0 +1,10 @@
+let stad = process.argv[2];
+
+console.log(stad);
+
+if (stad == "Amsterdam") {
+    console.log("Het is de hoofdstad van Nederland");
+} else {
+    console.log(`De stad is ${stad}`);
+}
+
