7dcd216102cb0c1ed1f5e5acfa2e6d7648af668b
unonewcreations <unonewcreations@gmail.com>
2023-07-29 15:07:02 +0200

    fixing nested git

---
+++ javascript-beginner/05-level/Of-nee-toch-niet-036c721f-c26bd279/script.js
@@ -0,0 +1,20 @@
+// array met  boodschappen
+let boodschappen = ['spaghetti', 'pesto', 'paprika'];
+
+// toevoegen wc-papier
+boodschappen.push('wc-papier');
+
+// print naar console
+console.log(boodschappen);
+
+// toevoegen pompoen
+boodschappen.push('pompoen');
+
+// print naar console
+console.log(boodschappen);
+
+// verwijder pompoen
+boodschappen.pop('pompoen');
+
+// print naar console
+console.log(boodschappen);
