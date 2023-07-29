06f7573c5b945f8fbfa0ace8a4657beb121496c0
unonewcreations <unonewcreations@gmail.com>
2023-07-29 15:05:29 +0200

    fixing nested git

---
+++ javascript-beginner/05-level/De-boodschappen-boodschappen-e16f53de-5ecf037e/script.js
@@ -0,0 +1,7 @@
+let voorraad = ['pesto', 'spaghetti', 'olijfolie'];
+console.log(voorraad);
+let groenten = ['courgette', 'knoflook', 'ui'];
+console.log(`het eerste product bij groenten is: ${groenten[0]}`);
+console.log(`het laatste product bij groenten is: ${groenten[2]}`);
+voorraad[1] = 'macaroni';
+console.log(voorraad);
