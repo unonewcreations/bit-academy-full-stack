9eab0011ea87113b9b2bb3b4d6ae80c1884a11a9
unonewcreations <unonewcreations@gmail.com>
2023-07-29 14:59:16 +0200

    fixing nested git

---
+++ javascript-beginner/04-level/Size-does-not-matter-b822ce51-34c9e99a/script.js
@@ -0,0 +1,5 @@
+let inwonersNl = parseInt(process.argv[2], 10);
+let inwonersDe = parseInt(process.argv[3], 10);
+
+console.log(`inwonersNL < inwonersDE: ${inwonersNl < inwonersDe}
+\ninwonersNL > inwonersDE: ${inwonersNl > inwonersDe}`);
