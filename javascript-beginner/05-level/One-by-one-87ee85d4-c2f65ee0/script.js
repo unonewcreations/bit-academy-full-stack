e397cc25e54fc35855357ed18184693af2d9923c
unonewcreations <unonewcreations@gmail.com>
2023-07-29 15:07:57 +0200

    fixing nested git

---
+++ javascript-beginner/05-level/One-by-one-87ee85d4-c2f65ee0/script.js
@@ -0,0 +1,7 @@
+// declare array
+let boodschappen = ['rijst', 'sperziebonen', 'tofu', 'sambal'];
+
+// loop and print variable
+for (let i = '0'; i < boodschappen.length; i++) {
+    console.log(`Biep! ${boodschappen[i]} gescand`);
+}
