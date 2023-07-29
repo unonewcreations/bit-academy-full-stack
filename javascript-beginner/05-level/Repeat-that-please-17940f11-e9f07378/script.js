8cac2381b8644e0b6e0d85505049678ecaad5a9f
unonewcreations <unonewcreations@gmail.com>
2023-07-29 15:08:58 +0200

    fixing nested git

---
+++ javascript-beginner/05-level/Repeat-that-please-17940f11-e9f07378/script.js
@@ -0,0 +1,9 @@
+// define variable
+let boodschappen = "amandelmelk";
+
+// loop variable 5x and print
+for (let i = '0'; i < 5; i++) {
+    if (boodschappen === "amandelmelk") {
+        console.log(`Biep! ${boodschappen} gescand`);
+    }
+}
