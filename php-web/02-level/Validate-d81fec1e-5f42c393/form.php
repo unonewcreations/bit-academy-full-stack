87324ca3ba9ed035ed4d6ec9b780261ba07017b9
unonewcreations <unonewcreations@gmail.com>
2023-07-30 10:52:16 +0200

    fixing nested git

---
+++ php-web/02-level/Validate-d81fec1e-5f42c393/form.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+
+<!-- header h1 -->
+<h1>Bit Academy - Form</h1>
+
+<!-- form -->
+<form method="post" action="">
+    <input type="text" name="email" placeholder="voer hier je email in!">
+    <button type="submit" name="submit">Verzenden</button>
+</form>
+
+<!-- php script -->
+<?php
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+} else {
+    exit;
+}
+
+// check id email is valid or not valid
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $show = "E-mail is geldig";
+} else {
+    $show = "E-mail is ongeldig";
+}
+?>
+
+<h2><?php echo $show ?></h2>
+
+</body>
+</html>
